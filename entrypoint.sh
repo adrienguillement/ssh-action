e4bf4cd8093778006725fa7d9c91fcc183d79b4e
GuillementAdrien <adrien.guillement@sephira.fr>
2021-08-12 16:06:42 +0200

    Fix multiline support

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,8 @@ set -eu
 export GITHUB="true"
 
 output=$(sh -c "/bin/drone-ssh $*")
+output="${output//'%'/'%25'}"
+output="${output//$'\n'/'%0A'}"
+output="${output//$'\r'/'%0D'}"
 
 echo "::set-output name=output::$output"

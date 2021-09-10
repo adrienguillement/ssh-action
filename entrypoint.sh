382ab2c198618f56711748f097babd6be201307a
Adrien Guillement <adrien.guillement@gmail.com>
2021-09-10 15:28:05 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,13 @@ export GITHUB="true"
 
 LOGFILE=$(echo `sh -c "/bin/drone-ssh $*"`)
 
-cat "$LOGFILE"
+echo "$LOGFILE"
 #cat "$LOGFILE"
 #cat "$LOGFILE"
 #cat "$LOGFILE"
 
-#output="${LOGFILE//'%'/'%25'}"
-#output="${LOGFILE//$'\n'/'%0A'}"
-#output="${LOGFILE//$'\r'/'%0D'}"
+output="${LOGFILE//'%'/'%25'}"
+output="${LOGFILE//$'\n'/'%0A'}"
+output="${LOGFILE//$'\r'/'%0D'}"
 
-#echo "::set-output name=output::$LOGFILE"
+echo "::set-output name=output::$LOGFILE"
